88db25e4223a47460122a8bf2bfe2d4b1f886f36
Ibai Arotçarena <arotcarena.ib@gmail.com>
2024-02-13 15:33:03 +0100

    modif config site_url

--- src/Config/SiteConfig.php
+++ src/Config/SiteConfig.php
@@ -3,5 +3,7 @@ namespace App\Config;
 
 class SiteConfig
 {
-    public const SITE_URL = 'https://localhost:8000';
+    // public const SITE_URL = 'https://localhost:8000';
+
+    public const SITE_URL = 'https://ia-portfolio-f661150a7212.herokuapp.com';
 }
